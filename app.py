1b000dc294c27fee82e72c781db475ceada75f9b
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2015-07-31 19:57:22 -0300

    init commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, jsonify
+import os, sys
+import ConfigParser
+import json
+
+
+# current dir
+dirname, filename = os.path.split(os.path.abspath(__file__))
+
+# py-asterisk
+sys.path.append(os.path.join(dirname,  'libs','py-asterisk'))
+from Asterisk.Manager import *
+
+
+app = Flask(__name__)
+
+# config file
+cfg_file = 'config.ini'
+cfg = ConfigParser.ConfigParser()
+cfg.read(os.path.join(dirname, cfg_file))
+
+def __connect_manager():
+    host = cfg.get('manager', 'host')
+    port = int(cfg.get('manager', 'port'))
+    user = cfg.get('manager', 'user')
+    password = cfg.get('manager', 'password')
+    return Manager((host, port), user, password)
+manager = __connect_manager()
+
+
+def get_data_queues():
+    data = manager.QueueStatus()
+    app.logger.info(data)
+    return data
+
+def parser_data_queue(data):
+    # convert references manager to string
+    for q in data:
+        for e in data[q]['entries']:
+            tmp = data[q]['entries'].pop(e)
+            data[q]['entries'][str(e)] = tmp
+            tmp = data[q]['entries'][str(e)]['Channel']
+            data[q]['entries'][str(e)]['Channel']  = str(tmp)
+
+    return data
+
+
+# ---------------------
+# ---- Routes ---------
+# ---------------------
+# home
+@app.route('/')
+def home():
+    data = get_data_queues()
+    return render_template('index.html', queues = data)
+
+# data queue
+@app.route('/queues')
+def queues():
+    data = parser_data_queue(get_data_queues())
+
+    return jsonify(
+        success = True,
+        data = data
+    )
+
+# ---------------------
+# ---- Main  ----------
+# ---------------------
+if __name__ == '__main__':
+#    __setLog()
+    app.debug = True
+    app.run(host='0.0.0.0')
