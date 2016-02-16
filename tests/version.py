de02d34a2e68b848472328ed3cd00b438ca33155
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-02-16 18:53:42 -0300

    init test directory

---
+++ tests/version.py
@@ -0,0 +1,6 @@
+from libs.qpanel import upgrader
+
+# Test
+print "stable version %s" %  get_stable_version()
+print "current version %s" % get_current_version()
+print "require upgrade? %s" % require_upgrade()
