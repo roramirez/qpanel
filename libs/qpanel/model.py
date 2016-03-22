046e19c35ac03624baa575f66e90b1b80e177cc2
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-03-22 17:11:57 -0300

    add functions to work with queue_log

--- libs/qpanel/model.py
+++ libs/qpanel/model.py
@@ -44,3 +44,72 @@ class QueueLog(DeclarativeBase):
                 'data4': self.data4,
                 'data5': self.data5
                }
+
+
+def queuelog_event_by_range_and_types(start_date, end_date, events=None,
+                                      agent=None, queue=None):
+    try:
+        q = session_db.query(QueueLog)
+        if from:
+            q = q.filter(QueueLog.time >= from)
+        if to:
+            q = q.filter(QueueLog.time <= to)
+        if events:
+            q = q.filter(QueueLog.event.in_(events))
+        if agent:
+            q = q.filter(QueueLog.agent.in_(agent))
+        if queue:
+            q = q.filter(QueueLog.queuename == queue)
+        return q.order_by(QueueLog.id.asc()).all()
+    except NoResultFound, e:
+        return None
+
+def queuelog_count_answered(start_date, end_date, agent=None, queue=None):
+    events = ['CONNECT']
+    data =  queuelog_event_by_range_and_types(start_date, end_date, events,
+                                              agent, queue)
+    return len(data)
+
+def queuelog_count_inbound(start_date, end_date, agent=None, queue=None):
+    events = ['ENTERQUEUE']
+    calls = []
+    data =  queuelog_event_by_range_and_types(start_date, end_date, events,
+                                              agent, queue)
+
+    for call in data:
+        if call.callid not in calls:
+            calls.append(call.callid)
+    return len(calls)
+
+def queuelog_count_abandon(start_date, end_date, agent=None, queue=None):
+    events = ['ABANDON']
+    data =  queuelog_event_by_range_and_types(start_date, end_date, events,
+                                              agent, queue)
+    return len(data)
+
+def queuelog_seconds_wait_abandon(start_date, end_date, agent=None, queue=None):
+    events = ['ABANDON']
+    seconds = 0
+    data =  queuelog_event_by_range_and_types(start_date, end_date, events,
+                                              agent, queue)
+    for call in data:
+        seconds = seconds + int(call.data3)
+    return seconds
+
+def queuelog_seconds_wait(start_date, end_date, agent=None, queue=None):
+    events = ['CONNECT']
+    seconds = 0
+    data =  queuelog_event_by_range_and_types(start_date, end_date, events,
+                                              agent, queue)
+    for call in data:
+        seconds = seconds + int(call.data1)
+    return seconds
+
+def queuelog_seconds_talking(start_date, end_date, agent=None, queue=None):
+    events = ['COMPLETECALLER', 'COMPLETEAGENT']
+    seconds = 0
+    data =  queuelog_event_by_range_and_types(start_date, end_date, events,
+                                              agent, queue)
+    for call in data:
+        seconds = seconds + int(call.data2)
+    return seconds
