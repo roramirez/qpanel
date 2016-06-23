63e5e7ac75d2916900f9c33162d3efbc28ff25dc
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-06-22 23:25:27 -0400

    add ROOT_PATH into settings

--- libs/qpanel/settings.py
+++ libs/qpanel/settings.py
@@ -1,5 +1,5 @@
 import os
 
 dirname, filename = os.path.split(os.path.abspath(__file__))
-PATH_FILE_CONFIG = os.path.join(dirname, os.pardir,
-                                os.pardir, 'config.ini')
+ROOT_PATH = os.path.join(dirname, os.pardir, os.pardir)
+PATH_FILE_CONFIG = os.path.join(ROOT_PATH, 'config.ini')
