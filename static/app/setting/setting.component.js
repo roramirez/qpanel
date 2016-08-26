54d4c616aa02db44a992c17309c328b699d79013
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-08-26 11:58:07 -0300

    fix if reset_stats and rename is undefined

--- static/app/setting/setting.component.js
+++ static/app/setting/setting.component.js
@@ -13,10 +13,14 @@ angular.
         });
 
         self.addResetStats = function() {
+            if (typeof self.settings.reset_stats === "undefined")
+                self.settings.reset_stats = [];
             self.settings.reset_stats.push({'name': null, 'value': null});
         }
 
         self.addRename = function() {
+            if (typeof self.settings.rename === "undefined")
+                self.settings.rename = [];
             self.settings.rename.push({'name': null, 'value': null});
         }
 
