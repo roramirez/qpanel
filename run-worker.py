0db234e11db1e4185a5dcbadcbfb74a0a14f1926
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2019-10-31 00:00:16 -0400

    Add Reset stats Python command program:
    This change add a simple script program to run the RQ worker to
    reset_stats. Include a sample configuration for using with supervisord

---
+++ run-worker.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2015-2019 Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
+#
+
+from qpanel import config, job, rq_worker
+
+
+if __name__ == '__main__':
+    """
+        Simple program to run worker for reset stats of Queue
+
+        If you are running the QPanel using a uwsgi script should use
+        this script to run background process will be reset stats for
+        the queues
+
+        see samples/resetstats_supervisor.conf
+    """
+
+    cfg = config.QPanelConfig()
+
+    if cfg.queues_for_reset_stats():
+        if job.check_connect_redis():
+            rq_worker.start_jobs()
+        else:
+            print("Error: There not connection to Redis")
+            print("       Reset stats will not work\n")
